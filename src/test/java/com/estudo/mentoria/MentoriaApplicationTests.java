ad8656f3aa484243aa3647e912f4b7dba74bb665
wilson.silva <wilson-gs@hotmail.com>
2023-10-03 13:40:27 -0300

    inicializando projeto, feito configurações iniciais, conexão com bd

---
+++ src/test/java/com/estudo/mentoria/MentoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudo.mentoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
